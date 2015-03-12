041afdd29c50ae1abd9447aa9446d373a3cadb20
Runar Heggset <r-heggse@online.no>
2015-03-12 13:19:55 +0100

    Avatar moves sort of

--- app/src/main/java/com/tdt4240/a19/mazegame/scenes/GameScene.java
+++ app/src/main/java/com/tdt4240/a19/mazegame/scenes/GameScene.java
@@ -1,13 +1,19 @@
 package com.tdt4240.a19.mazegame.scenes;
 
+import android.view.MotionEvent;
+
 import com.tdt4240.a19.mazegame.GameActivity;
 import com.tdt4240.a19.mazegame.GameState;
 import com.tdt4240.a19.mazegame.maze.MazeLayer;
 import com.tdt4240.a19.mazegame.user.UserLayer;
 
+import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
+import org.andengine.input.touch.TouchEvent;
 import org.andengine.util.color.Color;
+
+import java.util.Vector;
 
 /**
  * Created by Runar on 06.03.2015.
@@ -16,6 +22,8 @@ public class GameScene extends Scene {
 
     private UserLayer userLayer;
     private MazeLayer mazeLayer;
+
+    private Vector<Float> pressed = new Vector<Float>(2);
 
     public GameScene() {
         mazeLayer = new MazeLayer();
@@ -32,5 +40,28 @@ public class GameScene extends Scene {
 
         attachChild(mazeLayer);
         attachChild(userLayer);
+    }
+
+    @Override
+    public boolean onSceneTouchEvent(TouchEvent pSceneTouchEvent) {
+        switch (pSceneTouchEvent.getAction()) {
+            case TouchEvent.ACTION_DOWN:
+                pressed.clear();
+                pressed.add(pSceneTouchEvent.getX());
+                pressed.add(pSceneTouchEvent.getY());
+                break;
+            case TouchEvent.ACTION_UP:
+                float deltaX = pSceneTouchEvent.getX() - pressed.get(0);
+                float deltaY = pSceneTouchEvent.getY() - pressed.get(1);
+                if (Math.abs(deltaX) > Math.abs(deltaY))
+                    userLayer.getUser().setPosition(userLayer.getUser().getX() + deltaX, userLayer.getUser().getY());
+                else
+                    userLayer.getUser().setPosition(userLayer.getUser().getX(), userLayer.getUser().getY() + deltaY);
+                break;
+            case TouchEvent.ACTION_MOVE:
+                //userLayer.getUser().setPosition(pSceneTouchEvent.getX(), pSceneTouchEvent.getY());
+                break;
+        }
+        return false;
     }
 }
