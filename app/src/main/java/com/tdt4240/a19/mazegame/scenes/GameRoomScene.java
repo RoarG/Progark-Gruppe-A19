e2bafcb78d973972a833a38663c7ef969fe6957e
whintac <kjellgre@stud.ntnu.no>
2015-04-09 11:50:47 +0200

    edited gameRoomScene

--- app/src/main/java/com/tdt4240/a19/mazegame/scenes/GameRoomScene.java
+++ app/src/main/java/com/tdt4240/a19/mazegame/scenes/GameRoomScene.java
@@ -7,6 +7,10 @@ import com.tdt4240.a19.mazegame.assetsHandler.ResourcesManager;
 
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.scene.menu.MenuScene;
+import org.andengine.entity.scene.menu.item.IMenuItem;
+import org.andengine.entity.scene.menu.item.SpriteMenuItem;
+import org.andengine.entity.scene.menu.item.decorator.ScaleMenuItemDecorator;
 import org.andengine.entity.sprite.ButtonSprite;
 import org.andengine.entity.text.Text;
 import org.andengine.input.touch.TouchEvent;
@@ -15,62 +19,70 @@ import org.andengine.util.color.Color;
 /**
  * Created by Runar on 06.03.2015.
  */
-public class GameRoomScene extends BaseScene {
+public class GameRoomScene extends BaseScene implements org.andengine.entity.scene.menu.MenuScene.IOnMenuItemClickListener {
 
-    private boolean singlePlayer = false;
-    private ButtonSprite startButton;
-    //private CountDownTimer timer;
+    private MenuScene menuChildScene;
+    private final int MENU_START = 0;
+    private final int MENU_LEAVE = 1;
+
+    private void createMenuChildScene() {
+        setBackground(new Background(new Color(0.09804f, 0.6274f, 0.8784f)));
+        menuChildScene = new MenuScene(ResourcesManager.getInstance().camera);
+
+        final IMenuItem startMenuItem = new ScaleMenuItemDecorator(new SpriteMenuItem(MENU_START, ResourcesManager.getInstance().spriteHandler.getButtonSprite(), ResourcesManager.getInstance().vertexBufferObjectManager), 1.2f, 1);
+        final IMenuItem leaveMenuItem = new ScaleMenuItemDecorator(new SpriteMenuItem(MENU_LEAVE, ResourcesManager.getInstance().spriteHandler.getButtonSprite(), ResourcesManager.getInstance().vertexBufferObjectManager), 1.2f, 1);
+
+        Text startText = new Text(20.0f, 5.0f, ResourcesManager.getInstance().fontHandler.getBasicFont(), "Start Game", ResourcesManager.getInstance().vertexBufferObjectManager);
+        startMenuItem.attachChild(startText);
+        Text leaveText = new Text(20.0f, 5.0f, ResourcesManager.getInstance().fontHandler.getBasicFont(), "Leave Gameroom", ResourcesManager.getInstance().vertexBufferObjectManager);
+        leaveMenuItem.attachChild(leaveText);
+
+        menuChildScene.addMenuItem(startMenuItem);
+        menuChildScene.addMenuItem(leaveMenuItem);
+        menuChildScene.buildAnimations();
+        menuChildScene.setBackgroundEnabled(false);
+
+        startMenuItem.setPosition(startMenuItem.getX(), startMenuItem.getY() + 10);
+        leaveMenuItem.setPosition(leaveMenuItem.getX(), leaveMenuItem.getY() + 110);
+
+        menuChildScene.setOnMenuItemClickListener(this);
+
+        setChildScene(menuChildScene);
+    }
 
     @Override
     public void createScene() {
-        GameActivity game = ResourcesManager.getInstance().gameActivity;
-
-        setBackground(new Background(new Color(0.09804f, 0.6274f, 0.8784f)));
-        //final Text gameIsStarting = new Text(0, 0, game.getFontHandler().getBasicFont(), "Game Is Starting In:", "Game Is Starting In: XX".length(), game.getVertexBufferObjectManager());
-        //attachChild(gameIsStarting);
-
-        float x = 165.0f;
-        float y = 300.0f;
-
-        startButton = new ButtonSprite(x, y + 150.0f, ResourcesManager.getInstance().spriteHandler.getButtonSprite(), game.getVertexBufferObjectManager()) {
-            @Override
-            public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
-                super.onAreaTouched(pSceneTouchEvent, pTouchAreaLocalX, pTouchAreaLocalY);
-                //GameState.getInstance().getSettingsScene();
-                // startButton.setColor(new Color(0.09804f, 0.6274f, 0.8784f));
-                //timer = new CountDownTimer(10000, 1000) {
-                //@Override
-                //public void onTick(long millisUntilFinished) {
-                //gameIsStarting.setText("Game is starting in: " + millisUntilFinished / 1000);
-                //}
-
-                //@Override
-                //public void onFinish() {
-
-                //}
-                //}.start();
-                return true;
-            }
-        };
-        attachChild(startButton);
-        registerTouchArea(startButton);
-
-        Text startText = new Text(45.0f, 5.0f, ResourcesManager.getInstance().fontHandler.getBasicFont(), "Start game", game.getVertexBufferObjectManager());
-        startButton.attachChild(startText);
+        createMenuChildScene();
     }
 
     @Override
     public void onBackKeyPressed() {
-
+        SceneManager.getInstance().loadMenuScene();
+        //System.exit(0);
     }
 
     @Override
     public SceneManager.SceneType getSceneType() {
-        return null;
+        return SceneManager.SceneType.SCENE_MENUSCENE;
     }
 
     @Override
     public void disposeScene() {
 
     }
+
+    @Override
+    public boolean onMenuItemClicked(org.andengine.entity.scene.menu.MenuScene pMenuScene, IMenuItem pMenuItem, float pMenuItemLocalX, float pMenuItemLocalY) {
+        switch (pMenuItem.getID()) {
+            case MENU_START:
+                SceneManager.getInstance().loadGameScene(engine);
+                return true;
+            case MENU_LEAVE:
+                SceneManager.getInstance().loadMenuScene();
+                return true;
+            default:
+                return false;
+        }
+    }
+
 }
