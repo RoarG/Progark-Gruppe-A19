2c947d39e763c79d21e7e6c61224853f1c37a173
Roar Gjøvaag <Kafaenify@gmail.com>
2015-04-09 13:21:47 +0200

    Setup til MP

--- app/src/main/java/com/tdt4240/a19/mazegame/GameActivity.java
+++ app/src/main/java/com/tdt4240/a19/mazegame/GameActivity.java
@@ -3,11 +3,18 @@ package com.tdt4240.a19.mazegame;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.WindowManager;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.games.Games;
 import com.google.android.gms.games.multiplayer.Participant;
+import com.google.android.gms.games.multiplayer.realtime.RealTimeMessage;
+import com.google.android.gms.games.multiplayer.realtime.RealTimeMessageReceivedListener;
+import com.google.android.gms.games.multiplayer.realtime.Room;
+import com.google.android.gms.games.multiplayer.realtime.RoomConfig;
+import com.google.android.gms.games.multiplayer.realtime.RoomStatusUpdateListener;
+import com.google.android.gms.games.multiplayer.realtime.RoomUpdateListener;
 import com.google.android.gms.plus.Plus;
 
 import com.google.example.games.basegameutils.BaseGameUtils;
@@ -32,8 +39,13 @@ import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegion
 import org.andengine.ui.activity.BaseGameActivity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-public class GameActivity extends GBaseGameActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class GameActivity extends GBaseGameActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, RoomUpdateListener, RealTimeMessageReceivedListener, RoomStatusUpdateListener {
 
     public static final int CAMERA_WIDTH = 480;
     public static final int CAMERA_HEIGHT = 800;
@@ -47,6 +59,11 @@ public class GameActivity extends GBaseGameActivity implements GoogleApiClient.C
     */
 
     private static final String TAG = "MultiMazed";
+
+    // Request codes for the UIs that we show with startActivityForResult:
+    final static int RC_SELECT_PLAYERS = 10000;
+    final static int RC_INVITATION_INBOX = 10001;
+    final static int RC_WAITING_ROOM = 10002;
 
     // Request code used to invoke sign in user interactions.
     private static final int RC_SIGN_IN = 9001;
@@ -164,9 +181,7 @@ public class GameActivity extends GBaseGameActivity implements GoogleApiClient.C
         }));
         pOnPopulateSceneCallback.onPopulateSceneFinished();
         Log.w(TAG, "onPopulateScene");
-        //Connect google API
 
-        //mGoogleApiClient.connect();
 
     }
 
@@ -179,7 +194,7 @@ public class GameActivity extends GBaseGameActivity implements GoogleApiClient.C
        /* // register listener so we are notified if we receive an invitation to play
         // while we are in the game
         Games.Invitations.registerInvitationListener(mGoogleApiClient, this);
-
+// TODO: Ser næremere her
         if (connectionHint != null) {
             Log.d(TAG, "onConnected: connection hint provided. Checking for invite.");
             Invitation inv = connectionHint
@@ -243,6 +258,253 @@ public class GameActivity extends GBaseGameActivity implements GoogleApiClient.C
             SceneManager.getInstance().getCurrentScene().onBackKeyPressed();
         }
         return false;
+    }
+
+    // TODO: Legg til knapp for denne funksjonen
+    void startQuickGame() {
+        // quick-start a game with 1 randomly selected opponent
+        final int MIN_OPPONENTS = 1, MAX_OPPONENTS = 10;
+        Bundle autoMatchCriteria = RoomConfig.createAutoMatchCriteria(MIN_OPPONENTS,
+                MAX_OPPONENTS, 0);
+        RoomConfig.Builder rtmConfigBuilder = RoomConfig.builder(this);
+        rtmConfigBuilder.setMessageReceivedListener(this);
+        rtmConfigBuilder.setRoomStatusUpdateListener(this);
+        rtmConfigBuilder.setAutoMatchCriteria(autoMatchCriteria);
+        // TODO: Add loading screen while waiting
+        keepScreenOn();
+        resetGameVars();
+        Games.RealTimeMultiplayer.create(mGoogleApiClient, rtmConfigBuilder.build());
+
+    }
+    /*
+     * GAME LOGIC SECTION. Methods that implement the game's rules.
+     */
+
+    // Current state of the game:
+    int mSecondsLeft = -1; // how long until the game ends (seconds)
+    final static int GAME_DURATION = 20; // game duration, seconds.
+    int mScore = 0; // user's current score
+
+    // Reset game variables in preparation for a new game.
+    void resetGameVars() {
+        mSecondsLeft = GAME_DURATION;
+        mScore = 0;
+        // TODO: Implement: finn hva man må importere
+//        mParticipantScore.clear();
+//        mFinishedParticipants.clear();
+    }
+
+    // TODO: Trenger vi denne til å holde views?
+//    // This array lists all the individual screens our game has. GooglePlay
+//    final static int[] SCREENS = {
+//            R.id.screen_game, R.id.screen_main, R.id.screen_sign_in,
+//            R.id.screen_wait
+//    };
+//    int mCurScreen = -1;
+
+//    void switchToScreen(int screenId) {
+//        // make the requested screen visible; hide all others.
+//        for (int id : SCREENS) {
+//            findViewById(id).setVisibility(screenId == id ? View.VISIBLE : View.GONE);
+//        }
+//        mCurScreen = screenId;
+//
+//        // should we show the invitation popup?
+//        boolean showInvPopup;
+//        if (mIncomingInvitationId == null) {
+//            // no invitation, so no popup
+//            showInvPopup = false;
+//        } else if (mMultiplayer) {
+//            // if in multiplayer, only show invitation on main screen
+//            showInvPopup = (mCurScreen == R.id.screen_main);
+//        } else {
+//            // single-player: show on main screen and gameplay screen
+//            showInvPopup = (mCurScreen == R.id.screen_main || mCurScreen == R.id.screen_game);
+//        }
+//        findViewById(R.id.invitation_popup).setVisibility(showInvPopup ? View.VISIBLE : View.GONE);
+//    }
+
+
+    @Override
+    public void onRoomCreated(int i, Room room) {
+
+    }
+
+    @Override
+    public void onJoinedRoom(int i, Room room) {
+
+    }
+
+    @Override
+    public void onLeftRoom(int i, String s) {
+
+    }
+
+    @Override
+    public void onRoomConnected(int i, Room room) {
+
+    }
+
+    @Override
+    public void onRoomConnecting(Room room) {
+
+    }
+
+    @Override
+    public void onRoomAutoMatching(Room room) {
+
+    }
+
+    @Override
+    public void onPeerInvitedToRoom(Room room, List<String> strings) {
+
+    }
+
+    @Override
+    public void onPeerDeclined(Room room, List<String> strings) {
+
+    }
+
+    @Override
+    public void onPeerJoined(Room room, List<String> strings) {
+
+    }
+
+    @Override
+    public void onPeerLeft(Room room, List<String> strings) {
+
+    }
+
+    @Override
+    public void onConnectedToRoom(Room room) {
+
+    }
+
+    @Override
+    public void onDisconnectedFromRoom(Room room) {
+
+    }
+
+    @Override
+    public void onPeersConnected(Room room, List<String> strings) {
+
+    }
+
+    @Override
+    public void onPeersDisconnected(Room room, List<String> strings) {
+
+    }
+
+    @Override
+    public void onP2PConnected(String s) {
+
+    }
+
+    @Override
+    public void onP2PDisconnected(String s) {
+
+    }
+     /*
+     * COMMUNICATIONS SECTION. Methods that implement the game's network
+     * protocol.
+     */
+
+    // Score of other participants. We update this as we receive their scores
+    // from the network.
+    Map<String, Integer> mParticipantScore = new HashMap<String, Integer>();
+
+    // Participants who sent us their final score.
+    Set<String> mFinishedParticipants = new HashSet<String>();
+
+    // Called when we receive a real-time message from the network.
+    // Messages in our game are made up of 2 bytes: the first one is 'F' or 'U'
+    // indicating
+    // whether it's a final or interim score. The second byte is the score.
+    // There is also the
+    // 'S' message, which indicates that the game should start.
+    @Override
+    public void onRealTimeMessageReceived(RealTimeMessage rtm) {
+        byte[] buf = rtm.getMessageData();
+        String sender = rtm.getSenderParticipantId();
+        Log.d(TAG, "Message received: " + (char) buf[0] + "/" + (int) buf[1]);
+
+        if (buf[0] == 'F' || buf[0] == 'U') {
+            // score update.
+            int existingScore = mParticipantScore.containsKey(sender) ?
+                    mParticipantScore.get(sender) : 0;
+            int thisScore = (int) buf[1];
+            if (thisScore > existingScore) {
+                // this check is necessary because packets may arrive out of
+                // order, so we
+                // should only ever consider the highest score we received, as
+                // we know in our
+                // game there is no way to lose points. If there was a way to
+                // lose points,
+                // we'd have to add a "serial number" to the packet.
+                mParticipantScore.put(sender, thisScore);
+            }
+
+            // update the scores on the screen
+            // TODO: Update score view
+            //updatePeerScoresDisplay();
+
+            // if it's a final score, mark this participant as having finished
+            // the game
+            if ((char) buf[0] == 'F') {
+                mFinishedParticipants.add(rtm.getSenderParticipantId());
+            }
+        }
+    }
+
+    // Broadcast my score to everybody else.
+    void broadcastScore(boolean finalScore) {
+        if (!mMultiplayer)
+            return; // playing single-player mode
+
+        // First byte in message indicates whether it's a final score or not
+        mMsgBuf[0] = (byte) (finalScore ? 'F' : 'U');
+
+        // Second byte is the score.
+        mMsgBuf[1] = (byte) mScore;
+
+        // Send to every other participant.
+        for (Participant p : mParticipants) {
+            if (p.getParticipantId().equals(mMyId))
+                continue;
+            if (p.getStatus() != Participant.STATUS_JOINED)
+                continue;
+            if (finalScore) {
+                // final score notification must be sent via reliable message
+                Games.RealTimeMultiplayer.sendReliableMessage(mGoogleApiClient, null, mMsgBuf,
+                        mRoomId, p.getParticipantId());
+            } else {
+                // it's an interim score notification, so we can use unreliable
+                Games.RealTimeMultiplayer.sendUnreliableMessage(mGoogleApiClient, mMsgBuf, mRoomId,
+                        p.getParticipantId());
+            }
+        }
+    }
+
+    // TODO: Mangler UI section
+
+
+        /*
+     * MISC SECTION. Miscellaneous methods.
+     */
+
+
+    // Sets the flag to keep this screen on. It's recommended to do that during
+    // the
+    // handshake when setting up a game, because if the screen turns off, the
+    // game will be
+    // cancelled.
+    void keepScreenOn() {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    // Clears the flag that keeps the screen on.
+    void stopKeepingScreenOn() {
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 }
 
